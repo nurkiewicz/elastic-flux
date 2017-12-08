cd1088cf7b7418147841b01fe8985cd5b0c7dfe5
Tomasz Nurkiewicz <tomasz.nurkiewicz@allegrogroup.com>
2017-12-08 19:24:52 +0100

    Initial

---
+++ src/main/java/com/nurkiewicz/elasticflux/ElasticFluxApplication.java
@@ -0,0 +1,12 @@
+package com.nurkiewicz.elasticflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticFluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElasticFluxApplication.class, args);
+	}
+}
